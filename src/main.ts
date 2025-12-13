d4cfc4b13d3a4e5a21fca734d87f700a9a4d35f3
sugoidogo <sugoidogo@sugoidogo.com>
2025-12-13 04:42:09 -0800

    add install command, remove empty config check, add unknown command tolerance, add command descriptions

--- src/main.ts
+++ src/main.ts
@@ -23,15 +23,32 @@ program.name('snapback')
     .hook('preAction', function (program) {
         const config_file = program.opts().configFile
         if (fs.existsSync(config_file)) config = parseTOML(fs.readFileSync(config_file,'utf8'))
-        if (Object.keys(config).length === 0) {
-            console.warn('no subvolumes configured for snapback, exiting')
-            process.exit()
-        }
         const index_file = program.opts().indexFile
         if (fs.existsSync(index_file)) index = parseJSON(fs.readFileSync(index_file,'utf8'))
     })
 
+program.command('*').description('snapback ignores unknown commands')
+    .allowExcessArguments(true)
+    .allowUnknownOption(true)
+    .action(function () { console.warn('ignoring unknown command') })
+
+program.command('install').description('installs snapback on your system')
+    .action(function () {
+        let lib_snapper = '/usr/libexec/snapper'
+        if (!fs.existsSync(lib_snapper)) lib_snapper = '/usr/lib/snapper'
+        console.debug(`${import.meta.url} will be moved to ${lib_snapper}/99-snapback`)
+        const template = fs.readFileSync(import.meta.dirname + '/snapper-template.txt')
+        const service = fs.readFileSync(import.meta.dirname + '/snapper@.service')
+        fs.writeFileSync('/etc/systemd/system/snapper@.service', service, 'utf8')
+        fs.writeFileSync('/etc/snapper/config-templates/snapback', template, 'utf8')
+        fs.mkdirSync(lib_snapper + '/plugins', { recursive: true })
+        fs.renameSync(import.meta.url, lib_snapper + '/plugins/99-snapback')
+        sugoiSpawn('chmod', '+x', lib_snapper + '/plugins/99-snapback')
+        console.log('snapback installed')
+    })
+
 program.command('create-snapshot-post <subvolume> <fstype> <number>')
+    .description('create a backup after a snapshot')
     .action(function (subvolume, fstype, number) {
         if (!config[subvolume]) return
         const destination = strftime(config[subvolume])
@@ -44,6 +61,7 @@ program.command('create-snapshot-post <subvolume> <fstype> <number>')
     })
 
 program.command('delete-snapshot-post <subvolume> <fstype> <number>')
+    .description('delete a backup after deleting a snapshot')
     .action(function (subvolume, fstype, number) {
         const destination = index[subvolume][number]
         fs.unlinkSync(destination)
@@ -51,6 +69,7 @@ program.command('delete-snapshot-post <subvolume> <fstype> <number>')
     })
 
 program.command('delete-config-post <subvolume> <fstype>')
+    .description('delete metadata of deleted subvolume, retaining backups')
     .action(function (subvolume, fstype) {
         delete index[subvolume]
     })
